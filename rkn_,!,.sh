0ae55b4736abd9a26bf1ca7f712c7d2bc2da6e99
DanillMrzv <113212685+DanillMrzv@users.noreply.github.com>
2024-10-09 01:53:44 +0300

    Update rkn_,!,.sh

--- rkn_,!,.sh
+++ rkn_,!,.sh
@@ -5,24 +5,24 @@ mkdir -p ~/.cloudshell && touch ~/.cloudshell/no-apt-get-warning
 echo "Установка зависимостей..."
 sudo apt-get update -y --fix-missing && sudo apt-get install wireguard-tools jq -y --fix-missing
 
-priv="\${1:-\$(wg genkey)}"
-pub="\${2:-\$(echo "\${priv}" | wg pubkey)}"
+priv="${1:-\$(wg genkey)}"
+pub="${2:-\$(echo "\${priv}" | wg pubkey)}"
 api="https://api.cloudflareclient.com/v0i1909051800"
 ins() { curl -s -H 'user-agent:' -H 'content-type: application/json' -X "$1" "${api}/$2" "${@:3}"; }
-sec() { ins "\$1" "\$2" -H "authorization: Bearer $3" "${@:4}"; }
-response=\$(ins POST "reg" -d "{\"install_id\":\"\",\"tos\":\"\$(date -u +%FT%T.000Z)\",\"key\":\"\${pub}\",\"fcm_token\":\"\",\"type\":\"ios\",\"locale\":\"en_US\"}")
+sec() { ins "$1" "$2" -H "authorization: Bearer $3" "${@:4}"; }
+response=$(ins POST "reg" -d "{\"install_id\":\"\",\"tos\":\"\$(date -u +%FT%T.000Z)\",\"key\":\"\${pub}\",\"fcm_token\":\"\",\"type\":\"ios\",\"locale\":\"en_US\"}")
 
-id=\$(echo "$response" | jq -r '.result.id')
+id=$(echo "$response" | jq -r '.result.id')
 token=$(echo "$response" | jq -r '.result.token')
-response=$(sec PATCH "reg/\${id}" "$token" -d '{"warp_enabled":true}')
+response=$(sec PATCH "reg/${id}" "$token" -d '{"warp_enabled":true}')
 peer_pub=$(echo "$response" | jq -r '.result.config.peers[0].public_key')
 peer_endpoint=$(echo "$response" | jq -r '.result.config.peers[0].endpoint.host')
 client_ipv4=$(echo "$response" | jq -r '.result.config.interface.addresses.v4')
 client_ipv6=$(echo "$response" | jq -r '.result.config.interface.addresses.v6')
-port=$(echo "\$peer_endpoint" | sed 's/.*:$[0-9]*$\$/\1/')
-peer_endpoint=\$(echo "\$peer_endpoint" | sed 's/$.*$:[0-9]*/162.159.193.5/')
+port=$(echo "$peer_endpoint" | sed 's/.*:\([0-9]*\)$/\1/')
+peer_endpoint=$(echo "$peer_endpoint" | sed 's/:[0-9]*$/162.159.193.5/')
 
-conf=\$(cat <<-EOM
+conf=$(cat <<-EOM
 [Interface]
 PrivateKey = ${priv}
 S1 = 0
